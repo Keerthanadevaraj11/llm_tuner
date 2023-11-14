5065d965c3a57c6da119fea76c8faa1ab3724ca9
Keerthanadevaraj11 <78424315+Keerthanadevaraj11@users.noreply.github.com>
2023-11-14 12:24:05 +0530

    Update loader.py

--- llmtuner/dataset/loader.py
+++ llmtuner/dataset/loader.py
@@ -38,7 +38,7 @@ class Dataset:
             )
         else:
             common_voice["train"] = load_dataset(
-                "mozilla-foundation/common_voice_11_0", "en", split="train+validation"
+                "mozilla-foundation/common_voice_11_0", "en", split="validation"
             ).select(range(n_samples))
             common_voice["test"] = load_dataset(
                 "mozilla-foundation/common_voice_11_0", "en", split="test"
