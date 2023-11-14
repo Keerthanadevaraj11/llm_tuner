4c136e6e3895dd1b5fb4fe4d5032542458917fc6
Keerthanadevaraj11 <78424315+Keerthanadevaraj11@users.noreply.github.com>
2023-11-14 10:47:59 +0530

    Update whisper_model.py

--- llmtuner/models/whisper_model.py
+++ llmtuner/models/whisper_model.py
@@ -11,15 +11,26 @@ class WhisperModel(BaseModel):
         self.model = None
 
     def load(self):
-        if not self.model:
-            # Set the quantized model to use forced decoding
-            
-            self.model = WhisperForConditionalGeneration.from_pretrained(self.model_name_or_path)
-            self.model.config.forced_decoder_ids = None
-            self.model.config.suppress_tokens = []
-            self.model.is_peft_applied = False
-            self.is_peft_applied = self.model.is_peft_applied
+    if not self.model:
+        # Set the quantized model to use forced decoding
+        self.model = WhisperForConditionalGeneration.from_pretrained(self.model_name_or_path)
+
+        # Load a pre-trained WhisperProcessor model
+        processor = WhisperProcessor.from_pretrained("openai/whisper-small")
+
+        # Get the decoder prompt IDs for the English language
+        decoder_prompt_ids = processor.get_decoder_prompt_ids(language="en", task="transcribe")
+
+        # Set the forced decoder IDs
+        self.model.config.forced_decoder_ids = decoder_prompt_ids
+
+        # Set other configuration parameters
+        self.model.config.suppress_tokens = []
+        self.model.is_peft_applied = False
+
+        self.is_peft_applied = self.model.is_peft_applied
         return self.model
+
     
     def save(self, save_path, *args, **kwargs):
         self.model.save_pretrained(save_path, from_pt=True)
